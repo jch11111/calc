fad6a713c509be4c2ed00f9c23e3a93718cbc252
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2016-11-20 11:10:44 -0800

    added a getter for amount. displaying infinity for divide by zero

--- js/calc.js
+++ js/calc.js
@@ -1,13 +1,36 @@
 var calc = (function () {
 
     var total = 0,
-        currentEntry,
-        operationStack = [];
+        currentEntry = {
+            wholeAmount: 0,
+            decimalAmount: 0,
+            powerOfTen: null,
+            decimalMode: false,
+            isOperationsOnly: false
+        };
+        operationStack = [],
+        operatorSymbols = {
+            add: '+',
+            subtract: '-',
+            multiply: '×',
+            divide: '÷'
+        },
+        inifinitySymbol = '\u221E';
 
     var init = function () {
         $(function () {
             setEventHandlers();
             initializeCurrentEntry();
+            Object.defineProperty(currentEntry, 'amount', {
+                'get': function () {
+                    var currentEntry = this;
+                    if (!isNaN(currentEntry.wholeAmount) && !isNaN(currentEntry.decimalAmount)) {
+                        return currentEntry.wholeAmount + currentEntry.decimalAmount;
+                    } else {
+                        return NaN;
+                    }
+                }
+            });
             displayCurrentEntry();
         })
     };
@@ -19,24 +42,35 @@ var calc = (function () {
     }
 
     function initializeCurrentEntry() {
-        currentEntry = {
-            wholeAmount: 0,
-            decimalAmount: 0,
-            powerOfTen: null,
-            decimalMode: false
-        };
+        currentEntry.wholeAmount = 0;
+        currentEntry.decimalAmount = 0;
+        currentEntry.powerOfTen = null;
+        currentEntry.decimalMode = false;
+        currentEntry.isOperationsOnly = false;
     }
 
     function displayCurrentEntry() {
-        $('#results').text(currentEntry.wholeAmount + currentEntry.decimalAmount);
+        if (isNaN(currentEntry.amount)) {
+            display(inifinitySymbol);
+        } else {
+            display(currentEntry.amount);
+        }
+    }
+
+    function display(valueToDisplay) {
+        $('#results').text(valueToDisplay);
     }
 
     function handleButtonClick(buttonValueOrOperation) {
-        if (!isNaN(buttonValueOrOperation)) {
+        var isOPeration = isNaN(buttonValueOrOperation);
+        var isNumericAndAllowed = !isOPeration && !currentEntry.isOperationsOnly;
+
+        if (isNumericAndAllowed) {
             updateCurrentEntry(Number(buttonValueOrOperation));
             displayCurrentEntry();
-        } else {
-            performOperation(buttonValueOrOperation);
+        }
+        if (isOPeration) {
+            performOrCacheOperation(buttonValueOrOperation);
         }
     }
 
@@ -48,7 +82,7 @@ var calc = (function () {
         }
     }
 
-    function performOperation(operation) {
+    function performOrCacheOperation(operation) {
         switch (operation) {
             case "dot":
                 currentEntry.decimalMode = true;
@@ -69,16 +103,22 @@ var calc = (function () {
             case "subtract":
             case "multiply":
             case "divide":
-                operationStack.push(currentEntry.wholeAmount + currentEntry.decimalAmount);
+                operationStack.push(currentEntry.amount);
                 operationStack.push(operation);
                 initializeCurrentEntry();
+                displayOperator(operation);
                 break;
             case "equals":
                 setCurrentEntry(calculateStack());
                 operationStack = [];
                 displayCurrentEntry();
+                currentEntry.isOperationsOnly = true;
                 break;
         };
+    }
+
+    function displayOperator(operator) {
+        display(operatorSymbols[operator]);
     }
 
     function calculateStack() {
@@ -88,7 +128,7 @@ var calc = (function () {
             runningTotal = mathOperation(operationStack[stackItem], runningTotal, operationStack[stackItem + 1]);
         }
 
-        return mathOperation(operationStack[operationStack.length - 1], runningTotal, currentEntry.wholeAmount + currentEntry.decimalAmount);
+        return mathOperation(operationStack[operationStack.length - 1], runningTotal, currentEntry.amount);
     }
 
     function mathOperation(operation, x, y) {
